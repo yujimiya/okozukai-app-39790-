f25f11cdb8ac261bcb290d9663f1556f75bca4ac
yujimiya <yuji.miyamoto32@gmail.com>
2024-01-17 21:24:06 +0900

    events表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
   resources :users, only: [:edit, :update]
-  resources :events, only: [:index, :new]
+  resources :events, only: [:index, :new, :create, :show]
 end
