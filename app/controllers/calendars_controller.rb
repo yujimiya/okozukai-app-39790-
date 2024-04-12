fb7179eb884cbf05b88179eab268c7ac903e6ea2
yujimiya <yuji.miyamoto32@gmail.com>
2024-04-12 21:34:54 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,20 +1,20 @@
-class CalendarsController < ApplicationController
+# class CalendarsController < ApplicationController
   
-  def new
-  end
+#   def new
+#   end
 
-  def create
-    @calendar = Calendar.new(calendar_params)
-    if @calendar.save
-      ender json: { status: 'success', message: 'イベントが作成されました' }
-    else
-      render json: { status: 'error', message: 'イベントの作成に失敗しました' }, status: :unprocessable_entity
-    end
-  end
+#   def create
+#     @calendar = Calendar.new(calendar_params)
+#     if @calendar.save
+#       ender json: { status: 'success', message: 'イベントが作成されました' }
+#     else
+#       render json: { status: 'error', message: 'イベントの作成に失敗しました' }, status: :unprocessable_entity
+#     end
+#   end
 
-  private
+#   private
 
-  def event_params
-    params.require(:calendar).permit(:help_date)
-  end
-end
+#   def event_params
+#     params.require(:calendar).permit(:help_date)
+#   end
+# end
